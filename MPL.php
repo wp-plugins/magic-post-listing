07b507a5eb0e90e0c04effccf4e11515e477dd47
Webilia <Webilia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-24 13:23:32 +0000

    Release MPL1.8
    git-svn-id: https://plugins.svn.wordpress.org/magic-post-listing/tags/1.8@1229265 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ MPL.php
@@ -0,0 +1,28 @@
+<?php
+/**
+	Plugin Name: Magic Post Listing
+	Plugin URI: http://webilia.com
+	Description: An awesome plugin for listing the posts and creating posts/pages sliders
+	Author: Webilia Team
+	Version: 1.8
+	Author URI: http://webilia.com
+**/
+
+/** MPL Execution **/
+define('_WBMPLEXEC_', 1);
+
+/** Directory Separator **/
+if(!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
+
+/** MPL Absolute Path **/
+define('_WBMPL_ABSPATH_', dirname(__FILE__) .DS);
+
+/** Plugin Directory Name **/
+define('_WBMPL_BASENAME_', basename(_WBMPL_ABSPATH_));
+
+/** Include Webilia MPL class if not included before **/
+if(!class_exists('WBMPL')) require_once _WBMPL_ABSPATH_.'WB.php';
+
+/** Initialize Webilia MPL **/
+$WBMPL = WBMPL::instance();
+$WBMPL->init();
